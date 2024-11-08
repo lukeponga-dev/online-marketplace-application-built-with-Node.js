9ed2eadc8a2547221e57d5f25905a91a4cd8c0d6
lukeponga-dev <developmentdesignsltd@gmail.com>
2024-11-08 21:19:12 +1300

    3

--- backend/node_modules/backend/controllers/checkoutController.js
+++
@@ -1,7 +0,0 @@
-const checkout = (req, res) => {
-    const { cart, paymentDetails } = req.body;
-    // Payment processing logic here
-    res.send('Payment successful');
-};
-
-module.exports = { checkout };
